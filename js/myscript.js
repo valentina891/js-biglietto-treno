e57fb7a67a790366decc32dd3341a04eeb9b2f43
vale <valemil.vs@gmail.com>
2020-07-07 17:12:12 +0200

    cambio valori 0.8 e 0.6

--- js/myscript.js
+++ js/myscript.js
@@ -3,16 +3,18 @@ var etaUtente = parseInt(prompt("Inserisci la tua età"));
 
 var etaOver = 65;
 var etaMinorenni = 18;
+var scontoMinorenni = (100*20)/100;
+var scontoOver = (100*40)/100;
 var prezzoBiglietto = 0.21 * trattaUtente;
 document.getElementById('prezzo-biglietto').innerHTML = prezzoBiglietto;
 
 if (etaUtente < etaMinorenni) {
-    prezzoBiglietto = prezzoBiglietto * 0.8;
+    prezzoBiglietto = prezzoBiglietto * (100 - scontoMinorenni);
     document.getElementById('biglietto-over').innerHTML = prezzoBiglietto;
 }
 else if (etaUtente > etaOver) {
-    prezzoBiglietto = prezzoBiglietto * 0.6;
+    prezzoBiglietto = prezzoBiglietto * (100 - scontoOver);
     document.getElementById('biglietto-teen').innerHTML = prezzoBiglietto;
 }
 
-console.log(prezzoBiglietto);
+// console.log(prezzoBiglietto);
