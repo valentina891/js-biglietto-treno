edf54d727300cebfdeba38641c6aea5dfab4bcb4
vale <valemil.vs@gmail.com>
2020-07-07 16:30:29 +0200

    ultimo salvataggio

--- js/myscript.js
+++ js/myscript.js
@@ -1,7 +1,25 @@
 var trattaUtente = prompt("Km da percorrere");
 var etaUtente = parseInt(prompt("Inserisci la tua età"));
 
+var etaOver > 65;
+var etaMinorenni < 18;
+
+if (etaUtente<18) {
+    prezzoBiglietto * 0.8;
+}
+
+if (etaUtente>65) {
+    prezzoBiglietto * 0.6;
+}
+
+var scontatoMinorenni = 0.8;
+var scontatoOver = 0.6;
 
 var prezzoBiglietto = 0.21 * trattaUtente;
+var totaleViaggioOver = prezzoBiglietto * 0.6;
+var totaleViaggioMinorenni = prezzoBiglietto * 0.8;
 console.log(prezzoBiglietto);
-document.getElementById('biglietto-prezzo-pieno').innerHTML = prezzoBiglietto;
+
+// document.getElementById('biglietto-prezzo-pieno').innerHTML = prezzoBiglietto;
+// document.getElementById('biglietto-over').innerHTML = totaleViaggioOver;
+// document.getElementById('biglietto-teen').innerHTML = totaleViaggioMinorenni;
